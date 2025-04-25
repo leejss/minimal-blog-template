a47b158faa0bc9ea481e27ef55b857161d1bca91
leejss <goldemshine@gmail.com>
2025-04-25 17:03:36 +0900

    feat: add fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
+import { SITE } from "@/config";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: SITE.baseURL,
   vite: {
     resolve: {
       alias: {
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const SITE = {
+  title: "내 웹사이트",
+  description: "웹사이트 설명",
+  defaultLanguage: "ko",
+  baseURL: "https://example.com",
+  author: "작성자 이름",
+} as const;
